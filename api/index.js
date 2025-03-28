116dcb48c4699620302c5854f0ccdb2cad73ae66
𝗚𝗘𝗢𝗥𝗚𝗘 <george.methq@gmail.com>
2025-03-28 14:55:39 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,20 +1,16 @@
 const https = require('https');
-
 const VALID_TOKEN = "MO0XRD-ZRPL4O-9R0HJZ-9ZODEX-UMN7VW-3Q8FM2";
 module.exports = async (req, res) => {
     if (req.method !== 'GET') {
         return res.status(405).json({ error: "Method Not Allowed" });
     }
-
     const { uid, token } = req.query;
     if (!token || token !== VALID_TOKEN) {
         return res.status(401).json({ error: "Unauthorized: Invalid token" });
     }
-
     if (!uid) {
         return res.status(400).json({ error: "UID is required" });
     }
-
     const options = {
         hostname: 'graph.facebook.com',
         port: 443,
@@ -22,7 +18,6 @@ module.exports = async (req, res) => {
         method: 'GET',
         rejectUnauthorized: true,
     };
-
     https.get(options, (fbRes) => {
         if (fbRes.statusCode === 200 || fbRes.statusCode === 302) {
             res.status(200).json({ status: "Alive", message: "Account found." });
